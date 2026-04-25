f53c2f4299b87cda5ecfcd996732fe37d620d569
als228 <als228@lehigh.edu>
2026-04-25 17:05:40 -0400

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        def btrack(num_list, index):
+            if index >= len(res):
+                return
+            
+            res.append(list(num_list))
+            for j in range(1, len(nums)-index):
+                copy = list(num_list)
+                copy.append(nums[index+j])
+                btrack(copy, index+j)
+        
+        for i in range(len(nums)):
+            btrack([nums[i]], i)
+        
+        return res
