d3ed78fa29ef6795241a548b366985638cda8f59
als228 <als228@lehigh.edu>
2026-07-20 08:37:54 -0600

    Add: combination-target-sum-ii - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+
+        def btrack(index, remainder, seq):
+            if remainder == 0:
+                res.append(list(seq))
+                return
+            for i in range(index, len(candidates)):
+                if i > index and candidates[i] == candidates[i-1]:
+                    continue
+                if remainder - candidates[i] < 0:
+                    break
+                seq.append(candidates[i])
+                btrack(i+1, remainder-candidates[i], seq)
+                seq.pop()
+
+        btrack(0, target, [])
+        return res
